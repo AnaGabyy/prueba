5bdbc0bdae3b1c4f4b8ca49dffb63b0635090af8
AnaGabyy <ana.medina@davinci.edu.ar>
2023-10-02 15:15:46 +0000

    metodo acelerar

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ class Automovil:
     self.aceleracion=aceleracion
     self.velocidad=0
     
+  def acelerar(self):
+    self.velocidad = self.velocidad + self.aceleracion
+    velocidadAceleracion= f"La velocidad del coche es de {self.velocidad}"
+    return velocidadAceleracion
+  
+coche1=Automovil("Rojo", "Toyota", 20)
+
+print(coche1.ruedas, coche1.color, coche1.marca, coche1.aceleracion)
+print(f"El coche tiene una aceleracion de {coche1.aceleracion}")
+coche1.aceleracion=30
+
+
+coche1.acelerar()
+
+
+#print(f"La velocidad del coche es de {coche1.velocidad}")  
